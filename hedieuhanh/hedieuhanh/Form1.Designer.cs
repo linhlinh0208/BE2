b94952d9bcadf73a812c915d59638106fb91fa05
DELL <DELL@DESKTOP-ET451QD>
2024-10-17 23:23:07 +0700

    Add project files.

---
+++ hedieuhanh/hedieuhanh/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace hedieuhanh
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnbrowse = new Button();
+            btnstart = new Button();
+            btnrefresh = new Button();
+            btnstop = new Button();
+            label1 = new Label();
+            txtPath = new TextBox();
+            IbOutput = new ListBox();
+            SuspendLayout();
+            // 
+            // btnbrowse
+            // 
+            btnbrowse.Location = new Point(42, 49);
+            btnbrowse.Name = "btnbrowse";
+            btnbrowse.Size = new Size(75, 23);
+            btnbrowse.TabIndex = 0;
+            btnbrowse.Text = "Browse";
+            btnbrowse.UseVisualStyleBackColor = true;
+            btnbrowse.Click += btnbrowse_Click;
+            // 
+            // btnstart
+            // 
+            btnstart.Location = new Point(649, 50);
+            btnstart.Name = "btnstart";
+            btnstart.Size = new Size(75, 23);
+            btnstart.TabIndex = 1;
+            btnstart.Text = "Start";
+            btnstart.UseVisualStyleBackColor = true;
+            btnstart.Click += btnstart_Click;
+            // 
+            // btnrefresh
+            // 
+            btnrefresh.Location = new Point(50, 344);
+            btnrefresh.Name = "btnrefresh";
+            btnrefresh.Size = new Size(75, 23);
+            btnrefresh.TabIndex = 2;
+            btnrefresh.Text = "Refresh";
+            btnrefresh.UseVisualStyleBackColor = true;
+            btnrefresh.Click += btnrefresh_Click;
+            // 
+            // btnstop
+            // 
+            btnstop.Location = new Point(649, 350);
+            btnstop.Name = "btnstop";
+            btnstop.Size = new Size(75, 23);
+            btnstop.TabIndex = 3;
+            btnstop.Text = "Stop";
+            btnstop.UseVisualStyleBackColor = true;
+            btnstop.Click += btnstop_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(42, 94);
+            label1.Name = "label1";
+            label1.Size = new Size(128, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Cac process dang chay";
+            // 
+            // txtPath
+            // 
+            txtPath.Location = new Point(147, 50);
+            txtPath.Name = "txtPath";
+            txtPath.Size = new Size(478, 23);
+            txtPath.TabIndex = 5;
+            // 
+            // IbOutput
+            // 
+            IbOutput.FormattingEnabled = true;
+            IbOutput.ItemHeight = 15;
+            IbOutput.Location = new Point(42, 125);
+            IbOutput.Name = "IbOutput";
+            IbOutput.Size = new Size(698, 199);
+            IbOutput.TabIndex = 6;
+            IbOutput.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(IbOutput);
+            Controls.Add(txtPath);
+            Controls.Add(label1);
+            Controls.Add(btnstop);
+            Controls.Add(btnrefresh);
+            Controls.Add(btnstart);
+            Controls.Add(btnbrowse);
+            Name = "Form1";
+            Text = "Quan li process";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnbrowse;
+        private Button btnstart;
+        private Button btnrefresh;
+        private Button btnstop;
+        private Label label1;
+        private TextBox txtPath;
+        private ListBox IbOutput;
+    }
+}
---
+++ hedieuhanh/hedieuhanh/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+
+namespace hedieuhanh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnbrowse_Click(object sender, EventArgs e)
+        {
+            //tạo form duyệt chọn file khả thi cần chạy 
+            OpenFileDialog dlg = new OpenFileDialog();
+            //hiển thị form duyệt chọn file khả thi cần chạy 
+            DialogResult ret = dlg.ShowDialog();
+            //kiểm tra quyết ₫ịnh của người dùng, nếu người dùng chọn OK thì ghi nhận tên file 
+            if (ret == DialogResult.OK)
+                txtPath.Text = dlg.FileName;
+        }
+
+        private void btnstart_Click(object sender, EventArgs e)
+        {
+            
+            //tạo mới ₫ối tượng quản lý Process. 
+            Process myProcess = new Process();
+            try
+            {
+                //thiết lập ₫ường dẫn file cần chạy 
+                myProcess.StartInfo.FileName = txtPath.Text;
+                //kích hoạt process 
+                myProcess.Start();
+                //hiển thị danh sách các process ₫ang chạy 
+                DisplayProcess();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnstop_Click(object sender, EventArgs e)
+        {
+            //xác ₫ịnh tên, ID của process ₫ang ₫ược chọn ₫ể xóa 
+            String[] param = IbOutput.SelectedItem.ToString().Split(',');
+            //tìm ₫ối tượng quản lý process có ID xác ₫ịnh 
+            Process proc = Process.GetProcessById(Int32.Parse(param[1]));
+            //xóa process 
+            proc.Kill();
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+            //hiển thị lại danh sách các process ₫ang chạy hiện hành 
+            DisplayProcess();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void DisplayProcess()
+        {
+            //xóa nội dung cũ của ListBox 
+            IbOutput.Items.Clear();
+            //tìm danh sách các process ₫ang chạy 
+            Process[] plist = Process.GetProcesses();
+            //hiển thị tên, ID của từng process 
+            foreach (Process process in plist)
+            {
+                IbOutput.Items.Add(process.ProcessName + ", " + process.Id);
+            }
+
+        }
+    }
+}
